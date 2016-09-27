b3fe04456e2bcabc01f7119c62c7b18211357be2
Alexander Vladimirov <alexandervladimirov1902@gmail.com>
2016-09-27 16:27:05 +0300

    Server And Client Done.

--- singleserveruser/src/test/java/com/clouway/echoclient/ClientConnection.java
+++ singleserveruser/src/test/java/com/clouway/echoclient/ClientConnection.java
@@ -27,24 +27,24 @@ public class ClientConnection {
     @Test
     public void happyPath() throws IOException {
         DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-        String expected = "Server Response: Hello! " + dateFormat.format(new Date())+"\n";
+        String expected = "Server Response: Hello! " + dateFormat.format(new Date()) + "\n";
         Server server = new Server(new ServerSocket(6000));
         Thread serverThread = new Thread(server);
 
         serverThread.start();
 
         Client client = new Client(InetAddress.getLocalHost(), 6000);
-
         PrintStream originalOutput = new PrintStream(System.out);
         OutputStream outStream = new ByteArrayOutputStream();
         PrintStream prStream = new PrintStream(outStream);
         System.setOut(prStream);
 
-        client.connect();
+        client.run();
         prStream.close();
-
         System.setOut(originalOutput);
+
         String actual = outStream.toString();
+
         assertThat(actual, is(equalTo(expected)));
     }
 }
